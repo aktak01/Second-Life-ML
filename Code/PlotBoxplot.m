40b9c20d479be35789a36628f06660b3b17996e9
Aki Takahashi <akit2129@gmail.com>
2023-02-11 18:56:13 -0800

    Add files via upload

---
+++ Code/PlotBoxplot.m
@@ -0,0 +1,59 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright @ 2022 Stanford Energy Control Laboratory (PI: simona onori, sonori@stanford.edu), 
+% Stanford University. All Rights Reserved. 
+%
+% FILE CONTENT >> Plotting boxplots for observing model performance
+%
+% Developed by Aki Takahashi 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% set up work environment
+clearvars
+clc
+close all
+
+%% Create box plots
+
+close all 
+
+lco = load('LCO\LCO_Boxplot.mat');
+lfp = load('LFP\LFP_Boxplot.mat');
+nmc = load('NMC\NMC_Boxplot.mat');
+
+datasetLabels = ['NMC'; 'LCO'; 'LFP'];
+
+%plot results
+figure; box on; hold on; grid on;
+pos = [100 100 560 420];
+set(gcf, 'position', pos);
+boxplot([nmc.listRMSE, lco.listRMSE, lfp.listRMSE ], Labels = datasetLabels);
+ylabel('RMSPE [\%]', 'interpreter', 'latex');
+xlim([0 4]); ylim([0 5]);
+yticks([0:1:5]);
+set(gca,'Fontsize',20);
+set(findall(gcf,'-property','interpreter'),'interpreter','latex')
+set(findall(gcf,'-property','ticklabelinterpreter'),'ticklabelinterpreter','latex')
+
+figure; box on; hold on; grid on;
+pos2 = [100 100 560  420];
+set(gcf, 'position', pos2);
+boxplot([nmc.listMPE, lco.listMPE, lfp.listMPE ], Labels = datasetLabels);
+ylabel('MPE [\%]','interpreter','latex');
+xlim([0 4]); ylim([0 5]);
+yticks([0:1:5]);
+set(gca,'Fontsize',20);
+set(findall(gcf,'-property','interpreter'),'interpreter','latex')
+set(findall(gcf,'-property','ticklabelinterpreter'),'ticklabelinterpreter','latex')
+
+
+
+figure; box on; hold on; grid on;
+pos3 = [100 100 560  420];
+set(gcf, 'position', pos3);
+boxplot([nmc.listCalibration, lco.listCalibration, lfp.listCalibration ], Labels = datasetLabels);
+xlim([0 4]);
+ylabel('CS [-]','interpreter','latex');
+set(gca,'Fontsize',20);
+set(findall(gcf,'-property','interpreter'),'interpreter','latex')
+set(findall(gcf,'-property','ticklabelinterpreter'),'ticklabelinterpreter','latex')
+
---
+++ Code/PlotFeatures.m
@@ -0,0 +1,75 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright @ 2022 Stanford Energy Control Laboratory (PI: simona onori, sonori@stanford.edu), 
+% Stanford University. All Rights Reserved. 
+%
+% FILE CONTENT >> Plotting SOH vs. features
+%
+% Developed by Aki Takahashi 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% set up work environment
+clearvars
+clc
+close all
+%% specify settings for running program
+
+names = {'LFP'; 'NMC'; 'LCO'};
+
+name = names{3}; %choose chemistry
+
+if(strcmp(name, 'LFP'))
+    load('LFP/CCCVData_LFP.mat');
+    cellNum = 4;  
+elseif(strcmp(name, 'NMC'))
+    load('NMC/CCCVData_NMC.mat');
+    currList = {};
+    cellNum = 4;
+else
+    load('LCO/CCCVData_LCO.mat');  
+    cellNum = 7;
+end
+
+totalCells = length(socList);
+
+
+%%
+m = totalCells;
+P = 0.7;
+cutEnd = 0;
+cycleList = [];
+
+%eliminate current data for currList
+yTrain = cell2mat(socList(cellNum));
+if(strcmp(name, 'NMC'))
+    xTrain = [cell2mat(resList(cellNum,1:7)) ];   
+    rows = 2; 
+    featureNum = 7;
+else
+    xTrain = [cell2mat(resList(cellNum,1:7)) cell2mat(currList(cellNum,1:7)) ];   
+    rows = 3;
+    featureNum = 14;
+end
+
+load('heatmapLabels');
+
+%plot features in subplots
+figure
+for i = 1:featureNum
+    
+    subplot(rows,5,i);
+    scatter(xTrain(:,i), yTrain, 2);
+    xlabel(strjoin(['$\mathrm{' heatmapLabels(i) '}$']), 'interpreter', 'latex');
+    if(mod(i,5) == 1)
+        ylabel('SOH [\%]');
+    end    
+    box on
+    grid on
+    set(gca,'Fontsize',20);
+    xlim([-inf inf]);
+    
+end
+set(findall(gcf,'-property','interpreter'),'interpreter','latex')
+set(findall(gcf,'-property','ticklabelinterpreter'),'ticklabelinterpreter','latex')
+pos = [0 0 560*3.2   420*rows*1.5];
+set(gcf, 'position', pos);
+
